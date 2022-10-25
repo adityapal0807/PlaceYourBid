0574c3b12de75a43c6944f3e00a5e34b9f0b2888
Aditya Pal Singh <64854366+adityapal0807@users.noreply.github.com>
2022-10-25 16:22:04 +0530

    base dir

--- commerce/settings.py
+++ commerce/settings.py
@@ -121,9 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
- os.path.join(BASE_DIR,'auctions','static','auctions')   
-]
+
 
 STATIC_ROOT = os.path.join(BASE_DIR,'auctions','static','auctions')  
 
