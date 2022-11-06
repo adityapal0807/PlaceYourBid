404fc522fd0c91c971e0a5e3f895ee02bcda7968
Aditya Pal Singh <64854366+adityapal0807@users.noreply.github.com>
2022-11-06 15:29:21 +0530

    Urls.py fixed

--- commerce/urls.py
+++ commerce/urls.py
@@ -16,11 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from django.conf.urls.static import static
+from django.urls import re_path as url
+from django.views.static import serve
 from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("auctions.urls"))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+if DEBUG == False:
+    urlpatterns += url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
